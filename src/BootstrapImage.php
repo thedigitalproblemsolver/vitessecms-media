45338a8a469c7def8732e32c7fab8e5a48196d47
Jasper Jaklofsky <info@thedigitalproblemsolver.com>
2021-02-13 20:21:46 +0100

    Renamed folders to psr4

--- src/BootstrapImage.php
+++ src/BootstrapImage.php
@@ -2,29 +2,42 @@
 
 namespace VitesseCms\Media;
 
+use VitesseCms\Core\Utils\DebugUtil;
 use VitesseCms\Media\Helpers\BootstrapMediaService;
 
 error_reporting(E_ALL);
 ini_set('display_errors', '1');
 
-require_once __DIR__ . '/../../core/src/services/BootstrapService.php';
-require_once __DIR__ . '/services/BootstrapMediaService.php';
+require_once __DIR__ . '/../../core/src/Services/BootstrapService.php';
+require_once __DIR__ . '/../../core/src/CoreApplicaton.php';
+require_once __DIR__ . '/Services/BootstrapMediaService.php';
 require_once __DIR__ . '/../../core/src/AbstractInjectable.php';
-require_once __DIR__ . '/../../core/src/services/AbstractInjectableService.php';
-require_once __DIR__ . '/../../core/src/services/UrlService.php';
-require_once __DIR__ . '/../../core/src/utils/DirectoryUtil.php';
-require_once __DIR__ . '/../../core/src/utils/SystemUtil.php';
-require_once __DIR__ . '/../../core/src/utils/BootstrapUtil.php';
-require_once __DIR__ . '/../../core/src/services/CacheService.php';
-require_once __DIR__ . '/../../configuration/src/utils/AccountConfigUtil.php';
-require_once __DIR__ . '/../../configuration/src/utils/DomainConfigUtil.php';
-require_once __DIR__ . '/../../core/src/utils/DebugUtil.php';
-require_once __DIR__ . '/../../configuration/src/services/ConfigService.php';
+require_once __DIR__ . '/../../core/src/Services/AbstractInjectableService.php';
+require_once __DIR__ . '/../../core/src/Services/UrlService.php';
+require_once __DIR__ . '/../../core/src/Utils/DirectoryUtil.php';
+require_once __DIR__ . '/../../core/src/Utils/SystemUtil.php';
+require_once __DIR__ . '/../../core/src/Utils/BootstrapUtil.php';
+require_once __DIR__ . '/../../core/src/Services/CacheService.php';
+require_once __DIR__ . '/../../configuration/src/Utils/AccountConfigUtil.php';
+require_once __DIR__ . '/../../configuration/src/Utils/DomainConfigUtil.php';
+require_once __DIR__ . '/../../core/src/Utils/DebugUtil.php';
+require_once __DIR__ . '/../../configuration/src/Services/ConfigService.php';
+
+$cacheLifeTime = 604800;
+$useCache = $_SESSIONt['cache']??true;
+if (DebugUtil::isDocker($_SERVER['SERVER_ADDR'])) :
+    $cacheLifeTime = 1;
+    $useCache = false;
+endif;
 
 $bootstrap = new BootstrapMediaService();
 $bootstrap
     ->setSession()
-    ->setCache(__DIR__.'/../../../../cache/')
+    ->setCache(
+        __DIR__.'/../../../../cache/',
+        $useCache,
+        $cacheLifeTime
+    )
     ->setUrl()
     ->loadConfig()
     ->loaderSystem()
--- src/Helpers/VideoEmbeddHelper.php
+++ src/Helpers/VideoEmbeddHelper.php
@@ -41,7 +41,7 @@ class VideoEmbeddHelper
         if($video->_('videoId')) :
             return $view->renderTemplate(
                 self::getProvider(),
-                Di::getDefault()->get('config')->get('rootDir') . 'template/core/views/partials/video',
+                Di::getDefault()->get('config')->get('rootDir') . 'template/core/Views/partials/video',
                 ['video' => $video]
 
             );
