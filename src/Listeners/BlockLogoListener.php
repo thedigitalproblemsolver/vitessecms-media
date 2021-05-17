c616c4d6bb9fdbf30c64c43db28b3bbd7d8c6189
Jasper Jaklofsky <info@thedigitalproblemsolver.com>
2021-05-17 21:56:17 +0200

    Moved BlockLogo

---
+++ src/Listeners/BlockLogoListener.php
@@ -0,0 +1,29 @@
+<?php declare(strict_types=1);
+
+namespace VitesseCms\Media\Listeners;
+
+use Phalcon\Events\Event;
+use VitesseCms\Block\Forms\BlockForm;
+use VitesseCms\Form\Models\Attributes;
+use VitesseCms\Setting\Enum\CallingNameEnum;
+
+class BlockLogoListener
+{
+    public function buildBlockForm(Event $event, BlockForm $form): void
+    {
+        if (!$form->setting->has(CallingNameEnum::LOGO_DEFAULT)) :
+            $form->addFilemanager('Logo standaard', 'logo_default', (new Attributes())->setRequired(true));
+        endif;
+        if (!$form->setting->has(CallingNameEnum::LOGO_MOBILE)) :
+            $form->addFilemanager('Logo mobile', 'logo_mobile', (new Attributes())->setRequired(true));
+        endif;
+        if (!$form->setting->has(CallingNameEnum::LOGO_EMAIL)) :
+            $form->addFilemanager('Logo email', 'logo_email', (new Attributes())->setRequired(true));
+        endif;
+        if (!$form->setting->has(CallingNameEnum::FAVICON)) :
+            $form->addFilemanager('Favicon', 'favicon', (new Attributes())->setRequired(true));
+        endif;
+
+        $form->addToggle('Display motto', 'displayMotto');
+    }
+}
--- src/Listeners/InitiateAdminListeners.php
+++ src/Listeners/InitiateAdminListeners.php
@@ -4,11 +4,13 @@ namespace VitesseCms\Media\Listeners;
 
 use Phalcon\Events\Manager;
 use VitesseCms\Media\Blocks\Image;
+use VitesseCms\Media\Blocks\Logo;
 
 class InitiateAdminListeners
 {
     public static function setListeners(Manager $eventsManager): void
     {
         $eventsManager->attach(Image::class, new BlockImageListener());
+        $eventsManager->attach(Logo::class, new BlockLogoListener());
     }
 }
