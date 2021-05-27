10cee9984d6b70ca9434496895f6df3a1c00fe81
Jasper Jaklofsky <info@thedigitalproblemsolver.com>
2021-05-27 18:31:39 +0200

    Improved listeners

--- src/Listeners/Blocks/ImageListener.php
+++ src/Listeners/Blocks/ImageListener.php
@@ -1,11 +1,11 @@
 <?php declare(strict_types=1);
 
-namespace VitesseCms\Media\Listeners;
+namespace VitesseCms\Media\Listeners\Blocks;
 
 use Phalcon\Events\Event;
 use VitesseCms\Block\Forms\BlockForm;
 
-class BlockImageListener
+class ImageListener
 {
     public function buildBlockForm(Event $event, BlockForm $form): void
     {
