ac8841ecc11e7f91dc622078e96686c363de45cc
Jasper Jaklofsky <info@thedigitalproblemsolver.com>
2021-06-07 23:28:22 +0200

    Moved listeners

--- src/Listeners/Blocks/LogoListener.php
+++ src/Listeners/Blocks/LogoListener.php
@@ -5,7 +5,10 @@ namespace VitesseCms\Media\Listeners\Blocks;
 use Phalcon\Events\Event;
 use VitesseCms\Block\Forms\BlockForm;
 use VitesseCms\Form\Models\Attributes;
+use VitesseCms\Media\Blocks\Logo;
 use VitesseCms\Setting\Enum\CallingNameEnum;
+use VitesseCms\Setting\Enum\TypeEnum;
+use VitesseCms\Setting\Factory\SettingFactory;
 
 class LogoListener
 {
@@ -25,5 +28,50 @@ class LogoListener
         endif;
 
         $form->addToggle('Display motto', 'displayMotto');
+    }
+
+    public function beforeBlockSave(Event $event, Logo $logo): void
+    {
+        if (isset($logo->logo_default)) :
+            SettingFactory::create(
+                CallingNameEnum::LOGO_DEFAULT,
+                TypeEnum::IMAGE,
+                $logo->logo_default,
+                'Logo core',
+                true
+            )->save();
+        endif;
+
+        if (isset($logo->logo_mobile)) :
+            SettingFactory::create(
+                CallingNameEnum::LOGO_MOBILE,
+                TypeEnum::IMAGE,
+                $logo->logo_mobile,
+                'Logo mobile',
+                true
+            )->save();
+        endif;
+
+        if (isset($logo->logo_email)) :
+            SettingFactory::create(
+                CallingNameEnum::LOGO_EMAIL,
+                TypeEnum::IMAGE,
+                $logo->logo_email,
+                'Logo e-mail',
+                true
+            )->save();
+        endif;
+
+        if (isset($logo->favicon)) :
+            SettingFactory::create(
+                CallingNameEnum::FAVICON,
+                TypeEnum::IMAGE,
+                $logo->favicon,
+                'Logo favicon',
+                true
+            )->save();
+        endif;
+
+        unset($logo->logo_default, $logo->logo_mobile, $logo->logo_email, $logo->favicon);
     }
 }
