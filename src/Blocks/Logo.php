0014f778d1852459b8f4cdb5078b94e222869e8c
Jasper Jaklofsky <info@thedigitalproblemsolver.com>
2023-02-22 10:29:46 +0100

    CurrentItem bugfix

--- src/Blocks/Logo.php
+++ src/Blocks/Logo.php
@@ -9,13 +9,10 @@ class Logo extends AbstractBlockModel
 {
     public function getTemplateParams(Block $block): array
     {
-        $params = [
-            'block' => $block,
-            'uri' => $this->getDi()->get('url')->getBaseUri(),
-            'websiteName' => $this->getDi()->get('setting')->getString('WEBSITE_DEFAULT_NAME'),
-            'motto' => $this->getDi()->get('setting')->getString('SITE_LABEL_MOTTO')
-        ];
-
+        $params = parent::getTemplateParams($block);
+        $params['uri'] = $this->getDi()->get('url')->getBaseUri();
+        $params['websiteName'] = $this->getDi()->get('setting')->getString('WEBSITE_DEFAULT_NAME');
+        $params['motto'] = $this->getDi()->get('setting')->getString('SITE_LABEL_MOTTO');
 
         return $params;
     }
