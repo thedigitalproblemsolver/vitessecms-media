507845d52a1ef7e3412fdac67b686743a53ffc1f
thedigitalproblemsolver <github@thedigitalproblemsolver.nl>
2023-11-18 23:53:28 +0100

    Finetunerd podcast template

--- src/Utils/PodcastUtil.php
+++ src/Utils/PodcastUtil.php
@@ -6,7 +6,13 @@ namespace VitesseCms\Media\Utils;
 
 use Lukaswhite\PodcastFeedParser\Parser;
 use Phalcon\Di\Di;
+use stdClass;
+use VitesseCms\Admin\Helpers\PaginationHelper;
+use VitesseCms\Core\Enum\CacheEnum;
+use VitesseCms\Core\Enum\UrlEnum;
+use VitesseCms\Core\Services\CacheService;
 use VitesseCms\Core\Utils\UrlUtil;
+use VitesseCms\Mustache\DTO\RenderPartialDTO;
 use VitesseCms\Mustache\DTO\RenderTemplateDTO;
 use VitesseCms\Mustache\Enum\ViewEnum;
 
@@ -19,16 +25,38 @@ final class PodcastUtil
 
     public static function getEmbeddCode($url): string
     {
+        $evenstManager = Di::getDefault()->get('eventsManager');
+        /** @var CacheService $cacheService */
+        $cacheService = $evenstManager->fire(CacheEnum::ATTACH_SERVICE_LISTENER, new stdClass());
+        $cacheKey = $cacheService->getCacheKey($url);
+        $content = $cacheService->get($cacheKey);
+        if ($content === null) {
+            $content = file_get_contents($url);
+        }
+
         $parser = new Parser();
-        $parser->setContent(file_get_contents($url));
+        $parser->setContent($content);
         $podcasts = $parser->run();
 
-        return Di::getDefault()->get('eventsManager')->fire(
+        $pagination = new PaginationHelper(
+            $podcasts->getEpisodes()->getIterator(),
+            $evenstManager->fire(UrlEnum::ATTACH_SERVICE_LISTENER, new stdClass()),
+            Di::getDefault()->get('request')->get('offset', 'int') ?? 0
+        );
+
+        $paginationRendered = $evenstManager->fire(
+            ViewEnum::RENDER_PARTIAL_EVENT,
+            new RenderPartialDTO('bootstrap/pagination_helper', ['pagination' => $pagination])
+        );
+
+        return $evenstManager->fire(
             ViewEnum::RENDER_TEMPLATE_EVENT,
             new RenderTemplateDTO('podcasts', '', [
                 'description' => $podcasts->getDescription(),
                 'link' => $podcasts->getLink(),
-                'mostRecentEpisode' => $podcasts->getEpisodes()->mostRecent()
+                'mostRecentEpisode' => $podcasts->getEpisodes()->mostRecent(),
+                'episodes' => $pagination->getSliced(),
+                'pagination' => $paginationRendered
             ])
         );
     }
